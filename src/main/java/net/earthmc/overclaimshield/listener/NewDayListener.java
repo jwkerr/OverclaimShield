4f4f85e64cde92b5097a8f70d8af72e27141c19a
Fruitloopins <49851457+Fruitloopins@users.noreply.github.com>
2023-10-09 17:25:18 +1100

    Listener fixes

--- src/main/java/net/earthmc/overclaimshield/listener/NewDayListener.java
+++ src/main/java/net/earthmc/overclaimshield/listener/NewDayListener.java
@@ -5,11 +5,13 @@ import com.palmergames.bukkit.towny.event.NewDayEvent;
 import com.palmergames.bukkit.towny.object.Town;
 import net.earthmc.overclaimshield.Utils;
 import net.earthmc.overclaimshield.manager.TownMetadataManager;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 import java.time.Instant;
 
 public class NewDayListener implements Listener {
+    @EventHandler
     public void onNewDay(NewDayEvent event) {
         for (Town town : TownyAPI.getInstance().getTowns()) {
             if (!TownMetadataManager.hasOverclaimShield(town))
--- src/main/java/net/earthmc/overclaimshield/listener/TownPreClaimListener.java
+++ src/main/java/net/earthmc/overclaimshield/listener/TownPreClaimListener.java
@@ -2,15 +2,23 @@ package net.earthmc.overclaimshield.listener;
 
 import com.palmergames.bukkit.towny.event.TownPreClaimEvent;
 import com.palmergames.bukkit.towny.object.Town;
+import com.palmergames.bukkit.towny.object.TownBlock;
 import net.earthmc.overclaimshield.manager.TownMetadataManager;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 public class TownPreClaimListener implements Listener {
-    public onTownPreClaim(TownPreClaimEvent event) {
+    @EventHandler
+    public void onTownPreClaim(TownPreClaimEvent event) {
         if (!event.isOverClaim())
             return;
 
-        Town town = event.getTown();
+        TownBlock townBlock = event.getTownBlock();
+        Town town = townBlock.getTownOrNull();
+
+        if (town == null)
+            return;
+
         if (TownMetadataManager.hasOverclaimShield(town)) {
             event.setCancelled(true);
             event.setCancelMessage("Could not overclaim this plot as the town has purchased an overclaim shield");
